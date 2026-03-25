1df7d3698e71499a570ab7dde69a8578810aff10
jpraise2002-blip <jpraise2002@gmail.com>
2026-03-25 16:24:36 +0100

    added extra links to some elements on the side bar

--- slides.js
+++ slides.js
@@ -2,24 +2,104 @@
 const sideBar = document.getElementById('sideBar');
 const myLinks = document.getElementById('myLinks');
 const mainContent = document.getElementById('mainContent');
-const header = document.getElementById('header');
+const headerText = document.getElementById('headerText');
 const bookIcon = document.getElementById('bookIcon');
-const closebtn = document.getElementById('closebtn');
+const closeBtn = document.getElementById('closeBtn');
 
 sideBar.addEventListener('click', function () {
     if (myLinks.style.display === 'none') {
         myLinks.style.display = 'block';
         mainContent.style.display = 'none';
-        header.style.color = '#212529';
+        headerText.style.color = '#212529';
         bookIcon.style.color = '#ffffff';
         bookIcon.style.backgroundColor = 'black';
         closeBtn.style.display = 'block';
     } else {
         myLinks.style.display = 'none';
         mainContent.style.display = 'block';
-        header.style.color = '#ffffff';
+        headerText.style.color = '#ffffff';
         bookIcon.style.color = 'black';
         bookIcon.style.backgroundColor = '#ffffff';
         closeBtn.style.display = 'none';
+        destLink.style.display = 'none';
+        foodLink.style.display = 'none';
+        wellLink.style.display = 'none';
+        galLink.style.display = 'none';
     }
 });
+
+const destLink = document.getElementById('destLink');
+const foodLink = document.getElementById('foodLink');
+const wellLink = document.getElementById('wellLink');
+const galLink = document.getElementById('galLink');
+const exitBtn = document.getElementById('exitBtn');
+
+function destLinks() {
+    if (destLink.style.display === 'none') {
+        destLink.style.display = 'block';
+        myLinks.style.display = 'none';
+        foodLink.style.display = 'none';
+        wellLink.style.display = 'none';
+        galLink.style.display = 'none';
+        exitBtn.style.display = 'block';
+    } else {
+        destLink.style.display = 'none';
+        myLinks.style.display = 'block';
+    }
+};
+
+function foodLinks() {
+    if (foodLink.style.display === 'none') {
+        foodLink.style.display = 'block';
+        myLinks.style.display = 'none';
+        destLink.style.display = 'none';
+        wellLink.style.display = 'none';
+        galLink.style.display = 'none';
+        exitBtn.style.display = 'block';
+        
+    } else {
+        foodLink.style.display = 'none';
+        myLinks.style.display = 'block';
+        
+    }
+};
+
+function wellLinks() {
+    if (wellLink.style.display === 'none') {
+        wellLink.style.display = 'block';
+        myLinks.style.display = 'none';
+        destLink.style.display = 'none';
+        foodLink.style.display = 'none';
+        galLink.style.display = 'none';
+        exitBtn.style.display = 'block';
+        
+    } else {
+        wellLink.style.display = 'none';
+        myLinks.style.display = 'block';
+        
+    }
+};
+
+function galLinks() {
+    if (galLink.style.display === 'none') {
+        galLink.style.display = 'block';
+        myLinks.style.display = 'none';
+        destLink.style.display = 'none';
+        foodLink.style.display = 'none';
+        wellLink.style.display = 'none';
+        exitBtn.style.display = 'block';
+        
+    } else {
+        galLink.style.display = 'none';
+        myLinks.style.display = 'block';
+        
+    }
+};
+
+function goBack() {
+    if (myLinks.style.display === 'none') {
+        myLinks.style.display = 'block';
+        exitBtn.style.display = 'none';
+    }
+}
+
